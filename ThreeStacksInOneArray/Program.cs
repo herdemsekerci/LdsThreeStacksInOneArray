0c0f43b8adc1050f53cf9244309949ac9d289a1f
Erdem Şekerci -local <herdemsekerci@gmail.com>
2020-09-28 11:41:53 +0300

    Initial commit

---
+++ ThreeStacksInOneArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ThreeStacksInOneArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TrippleStack trippleStack = new TrippleStack(10);
+
+            // pushing items into stack1
+            trippleStack.Push(TrippleStack.StackId.StackOne, "erdem şekerci");
+            trippleStack.Push(TrippleStack.StackId.StackOne, 118);
+            trippleStack.Push(TrippleStack.StackId.StackOne, 1f);
+
+            // pushing items into stack2
+            trippleStack.Push(TrippleStack.StackId.StackTwo, "Test stack2 elemanı");
+            trippleStack.Push(TrippleStack.StackId.StackTwo, "123456");
+            trippleStack.Push(TrippleStack.StackId.StackTwo, 8.5);
+            trippleStack.Push(TrippleStack.StackId.StackTwo, "loodos");
+
+            // pushing items into stack3
+            trippleStack.Push(TrippleStack.StackId.StackThree, "Stack 3");
+            trippleStack.Push(TrippleStack.StackId.StackThree, "test");
+
+            Console.WriteLine($"Stack1 last item: {trippleStack.Peek(TrippleStack.StackId.StackOne)}, stack1 current size: {trippleStack.Count(TrippleStack.StackId.StackOne)}");
+            Console.WriteLine($"Stack 2 last item: {trippleStack.Peek(TrippleStack.StackId.StackTwo)}, stack2 current size: {trippleStack.Count(TrippleStack.StackId.StackTwo)}");
+            Console.WriteLine($"Stack 3 last item: {trippleStack.Peek(TrippleStack.StackId.StackThree)}, stack3 current size: {trippleStack.Count(TrippleStack.StackId.StackThree)}");
+
+        }
+    }
+}
---
+++ ThreeStacksInOneArray/TrippleStack.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace ThreeStacksInOneArray
+{
+    public class TrippleStack
+    {
+        public enum StackId
+        {
+            StackOne,
+            StackTwo,
+            StackThree
+        }
+        private object[] values;
+        private Dictionary<StackId, List<int>> stackIndex;
+
+        public TrippleStack(int defaultStackCapacity)
+        {
+            values = new object[defaultStackCapacity * 3];
+            stackIndex = new Dictionary<StackId, List<int>>();
+            // stack 1
+            stackIndex.Add(StackId.StackOne, new List<int>());
+            // stack 2
+            stackIndex.Add(StackId.StackTwo, new List<int>());
+            // stack 3
+            stackIndex.Add(StackId.StackThree, new List<int>());
+        }
+
+        private int GetAvailableIndex()
+        {
+            if (values.Any(a => a == null))
+                return Array.FindIndex(values, v => v == null);
+            else
+                return -1; // array full
+        }
+
+        private bool StackHasItem(StackId stackId)
+        {
+            return stackIndex[stackId].Count > 0;
+        }
+
+        public int Count(StackId stackId)
+        {
+            return stackIndex[stackId].Count;
+        }
+
+        public void Push(StackId stackId, object value)
+        {
+            int availableIndex = GetAvailableIndex();
+
+            if (availableIndex == -1)
+            {
+                // array full
+                throw new Exception("Array is full!");
+            }
+            else
+            {
+                values[availableIndex] = value;
+                stackIndex[stackId].Add(availableIndex);
+
+            }
+
+        }
+
+        public object Pop(StackId stackId)
+        {
+            if (StackHasItem(stackId))
+            {
+                int lastIndexOfStack = stackIndex[stackId].Last();
+                object currentItem = values[lastIndexOfStack];
+                values[lastIndexOfStack] = null; // remove item from array which is the last item of current stack
+                stackIndex[stackId].RemoveAt(stackIndex[stackId].Count - 1); // remove the last item index from the index list
+
+                return currentItem;
+            }
+            else
+            {
+                throw new Exception("Stack has no item to pop.");
+            }
+        }
+
+        public object Peek(StackId stackId)
+        {
+            if (!StackHasItem(stackId))
+            {
+                // stack has no items
+                return null;
+            }
+
+            int lastElemenentIndexOfStack = stackIndex[stackId].Last();
+            return values[lastElemenentIndexOfStack];
+        }
+    }
+
+}
